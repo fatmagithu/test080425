8cb5ddcaa198e9a8aeeb7a7338034bc736d03a4b
fatmagithu <fatma.asserrar@laposte.net>
2025-04-08 00:16:26 +0200

    modif1

---
+++ test080425.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cours git</title>
+</head>
+<body>
+    <h1>cours git</h1>  
+<p>bonjour</p>  
+    <p>bonjour2</p>
+    <p>bonjour3</p> 
+
+</body>
+</html>
